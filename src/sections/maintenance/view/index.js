e026391608663aede961a48a4e87f436651d47c1
1rubiz <1izekorruby@gmail.com>
2025-08-16 10:56:24 +0100

    fix export default

--- src/sections/maintenance/view/index.js
+++ src/sections/maintenance/view/index.js
@@ -3,5 +3,5 @@ export { default as UserCardsView } from './user-cards-view';
 export { default as UserCreateView } from './user-create-view'; // new 
 export { default as UserProfileView } from './user-profile-view';
 export { default as UserDetailsView } from './user-details-view';
-export { default as InventoryListView } from './inventory-list-view'; // list
+export { default as MaintenanceView } from './inventory-list-view'; // list
 export { default as RequestInventory } from './request-inventory-edit';
